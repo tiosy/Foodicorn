06c0c2c5200bb588667d8bab5deaa389eb7ad560
Timothy Yeh <timothy.yeh@gmail.com>
2015-04-13 13:33:46 -0700

    Initial Commit

---
+++ Foodicorn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Foodicorn
+//
+//  Created by tim on 4/13/15.
+//  Copyright (c) 2015 Timothy Yeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
